01387dacb9b07bf08ba3f02c824f01aee4cf57e7
AbhishekBhakari <bhakariabhi@gmail.com>
2025-06-25 21:02:30 +0530

    change the maeque content

--- components/Marque.tsx
+++ components/Marque.tsx
@@ -2,18 +2,20 @@
 import React from "react";
 
 const items: string[] = [
-  "Facebook Ads",
-  "Google Ads",
-  "Instagram Ads",
-  "LinkedIn Ads",
-  "YouTube Ads",
-  "Twitter Ads",
+  "Equity Research",
+  "Fundamental Analysis",
+  "Technical Analysis",
+  "Investment Advisory",
+  "Portfolio Review",
+  "Market Insights",
 ];
 
 const RunningStrip: React.FC = () => {
   return (
     <div className="overflow-hidden bg-white py-3 border-y border-gray-300">
-        <div className="text-black font-light text-md px-6 tracking-wide text-center">OUR EXPERTISE</div>
+      <div className="text-black font-light text-md px-6 tracking-wide text-center">
+        OUR EXPERTISE
+      </div>
       <div className="flex p-8 animate-marquee whitespace-nowrap">
         {[...items, ...items].map((item, index) => (
           <span
